105b78242fae2b36e40fd2f71fe9497caa6561c8
Svend Tofte <stofte@gmail.com>
2017-08-17 19:05:54 +0200

    Add procmon to appveyor build

--- DumpDlls.ps1
+++ DumpDlls.ps1
@@ -1,7 +1,7 @@
 # Runs ProccessMonitor to capture all dlls accessed by dotnet.exe when building solution.
 # Provides a starting point for MetadataReferences required when building using Roslyn.
 dotnet clean ConsoleApp1 -nologo
-start-process "procmon.exe" -ArgumentList "/quiet","/minimized","/loadconfig procmon-conf.pmc","/backingfile data.pml"
+start-process "procmon.exe" -ArgumentList "/accepteula","/quiet","/minimized","/loadconfig procmon-conf.pmc","/backingfile data.pml"
 start-process "procmon.exe" -ArgumentList "/waitforidle" -wait
 dotnet build ConsoleApp1
 start-process "procmon.exe" -ArgumentList "/terminate" -wait
