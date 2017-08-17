eb0662d475ec47cfe5dc66f1bffddeb9492516ea
Svend Tofte <stofte@gmail.com>
2017-08-17 19:29:31 +0200

    CI

--- DumpDlls.ps1
+++ DumpDlls.ps1
@@ -7,9 +7,11 @@ dotnet restore ConsoleApp1
 write-host "Starting procmon"
 start-process "procmon.exe" -ArgumentList "/accepteula","/quiet","/minimized","/loadconfig procmon-conf.pmc","/backingfile data.pml"
 start-process "procmon.exe" -ArgumentList "/accepteula","/waitforidle" -wait
+write-host "Starting build"
 dotnet build ConsoleApp1
 start-process "procmon.exe" -ArgumentList "/accepteula","/terminate" -wait
 start-process "procmon.exe" -ArgumentList "/accepteula","/openlog","data.pml","/saveapplyfilter","/saveas data.csv" -wait
+write-host "Starting node"
 node process-raw.js data.csv > ConsoleApp1\ConsoleApp1\dlls.json
 remove-item (Get-Item data.csv)
 remove-item (Get-Item *.pml) # might generate more then one pml file
