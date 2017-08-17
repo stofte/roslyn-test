6f0ce7a856926dc76beba42f97ed63b3c2f8b867
Svend Tofte <stofte@gmail.com>
2017-08-17 19:24:12 +0200

    CI

--- DumpDlls.ps1
+++ DumpDlls.ps1
@@ -1,11 +1,15 @@
 # Runs ProccessMonitor to capture all dlls accessed by dotnet.exe when building solution.
 # Provides a starting point for MetadataReferences required when building using Roslyn.
-dotnet clean ConsoleApp1 -nologo
+write-host "Starting clean"
+dotnet clean ConsoleApp1
+write-host "Starting restore"
+dotnet restore ConsoleApp1
+write-host "Starting procmon"
 start-process "procmon.exe" -ArgumentList "/accepteula","/quiet","/minimized","/loadconfig procmon-conf.pmc","/backingfile data.pml"
-start-process "procmon.exe" -ArgumentList "/waitforidle" -wait
+start-process "procmon.exe" -ArgumentList "/accepteula","/waitforidle" -wait
 dotnet build ConsoleApp1
-start-process "procmon.exe" -ArgumentList "/terminate" -wait
-start-process "procmon.exe" -ArgumentList "/openlog","data.pml","/saveapplyfilter","/saveas data.csv" -wait
+start-process "procmon.exe" -ArgumentList "/accepteula","/terminate" -wait
+start-process "procmon.exe" -ArgumentList "/accepteula","/openlog","data.pml","/saveapplyfilter","/saveas data.csv" -wait
 node process-raw.js data.csv > ConsoleApp1\ConsoleApp1\dlls.json
 remove-item (Get-Item data.csv)
 remove-item (Get-Item *.pml) # might generate more then one pml file
